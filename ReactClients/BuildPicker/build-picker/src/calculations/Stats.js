f9f08e078a345513ac9f8b919620579fce5c0cd8
Eric Spaulding <espaulding@users.noreply.github.com>
2022-07-02 00:21:45 -0600

    tweaking attack speed

--- ReactClients/BuildPicker/build-picker/src/calculations/Stats.js
+++ ReactClients/BuildPicker/build-picker/src/calculations/Stats.js
@@ -257,17 +257,12 @@ const LostArkMath = {
     // but it's equally unreasonable to assume 100% of attack speed bonus is a dps increase
     // give up to a 60% boost for bonus atk speed based on CDR
     normalizeAtkSpeed: function(dmg, atkSpeed, cdr) {
-        var percentBonusAtkSpeed = .1;
-        var cdrCap = .75;
-        var bonus = percentBonusAtkSpeed;
-            bonus *= ((1 - cdr) / cdrCap); 
+        var bonus = .45, cdrCap = .75;
+            //bonus *= ((1 - cdr) / cdrCap); 
         var bonusAtkSpeed = (atkSpeed > 1) ? (atkSpeed - 1) : 0;
         //console.log('bonus before ' + bonus + ' speed: ' + bonusAtkSpeed);
         bonus = (bonus + 1) * bonusAtkSpeed;
-        //console.log('dmg: ' + dmg);
-        //console.log('bonus after ' + bonus);
-        if (atkSpeed >= 1 && cdr > 0) { dmg *= (bonus / 3 + 1); }
-        //console.log('dmg: ' + dmg);
+        if (atkSpeed >= 1 && cdr > 0) { dmg *= (bonus / 4 + 1); }
         return dmg;
     },
 
